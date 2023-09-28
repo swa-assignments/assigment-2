9fba039776e7f66ea3335aaaff4b4f013215a8b1
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-09-28 11:11:19 +0200

    Initiation

---
+++ Assigment 2/template/oo/src/board.ts
@@ -0,0 +1,34 @@
+export type Generator<T>= { next:() => T } 
+
+export type Position = {
+    row: number,
+    col: number
+}
+
+export type Match<T> = {
+    matched: T,
+    positions: Position[]
+}
+export type Effect<T> = {
+    type : "string";
+    position :"Position"
+
+}
+
+export type BoardEvent<T> = ?;
+
+export type BoardListener<T> = ?;
+
+export class Board<T> {
+    addListener(listener: BoardListener<T>) {
+    }
+
+    piece(p: Position): T | undefined {
+    }
+
+    canMove(first: Position, second: Position): boolean {
+    }
+    
+    move(first: Position, second: Position) {
+    }
+}
